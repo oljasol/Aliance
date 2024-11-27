d4569d1b759c1be626c678a7e769548b3d590c7f
Ольга Солопова <oljasol@inbox.ru>
2024-11-27 17:03:32 +0300

    add: functions lightModeбщзут and close menu

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@ const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo-svg use ');
 const mMenuToggle = document.querySelector('.mobile-menu-toggle ');
 const menu = document.querySelector('.mobile-menu ');
-window.addEventListener('scroll', () => {
- if (this.scrollY >1) {
+const lightModeOn=(event)=>{
   navbar.classList.add("navbar-light");
   logo.href.baseVal="img/sprite.svg#logo";
- } else {
- navbar.classList.remove("navbar-light");
- logo.href.baseVal="img/sprite.svg#logo-light";
- }
+};
+const lightModeOff=(event)=>{
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal="img/sprite.svg#logo-light";
+};
+const openMenu=(event)=> {
+  menu.classList.add("is-open");// функция открывания меню,вешает класс is-open
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow="hidden";//запрещаем прокрутку сайта под меню
+  lightModeOn();
+};
+const closeMenu=(event)=> {
+  menu.classList.remove("is-open");// функция закрывания меню,вешает класс is-open
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow="";//возвращает прокрутку сайта под меню
+  lightModeOff();
+};
+
+window.addEventListener('scroll', () => {this.scrollY >1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) =>{
   event.preventDefault();
-  menu.classList.toggle("is-open")
+menu.classList.contains("is-open") ? closeMenu() : openMenu();
 })
